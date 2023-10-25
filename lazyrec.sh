3ad30329cf6af9808c4e7823424dadda1c3ed02c
Gabriel Pedro <gabrielpjehnen@hotmail.com>
2023-10-25 13:06:04 -0300

    abc

---
+++ lazyrec.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+#Author: Gabriel Jehnen
+
+SCOPE_LIST=$1
+FOLDER_NAME=$2
+echo $FOLDER_NAME
+USERNAME=$(whoami)
+echo $USERNAME
+FOLDER_PATH=/home/$USERNAME/$FOLDER_NAME
+echo $FOLDER_PATH
+
+mkdir $FOLDER_PATH
+mv $1 $FOLDER_PATH
+cd $FOLDER_PATH
+
+configure_gf(){
+    cd ~; mkdir .gf; cd .gf;
+    git clone https://github.com/1ndianl33t/Gf-Patterns.git;
+    cd Gf-Patterns; mv * ..; rm -r Gf-Patterns;
+}
+
+install_tools() {
+    author="$1"
+    shift
+    for tool in "$@"; do
+        tool_name=$(echo $tool | awk -F/ '{print $NF}')
+        if ! command -v "$tool_name" &> /dev/null; then
+            if $tool_name == "naabu"; then
+                sudo apt install -y libcap-dev
+            elif $tool_name == "gf"; then
+                configure_gf
+            fi
+            echo "$tool_name não está instalado. Instalando..."
+            go install -v "github.com/$author/$tool@latest"
+        fi
+    done
+}
+
+subdomain_enum(){
+    echo -e "Starting Subdomain Enumeration (1/6)\n"
+    subfinder -dL $SCOPE_LIST -o subfinder.txt &
+    cat $SCOPE_LIST | assetfinder > assetfinder.txt &
+    wait
+    sort -u -o all_subs.txt subfinder.txt assetfinder.txt
+    cat all_subs.txt | httprobe > active_urls.txt
+    rm assetfinder.txt; rm subfinder.txt; rm all_subs.txt
+    echo -e "Finished Subdomain Enumeration\n"
+}
+
+verify_subtakeover(){
+    echo -e "Verifying Urls vulnerable to Subdomain Takeover (2/6)\n"
+    subzy run --targets active_urls.txt
+    echo -e "Finished Verifying Subs Takeover\n"
+}
+
+gather_urls(){
+    echo -e "Gathering Url's... This may take a while (\n"
+    cat active_urls.txt | gau --threads 5 > gau.txt
+}
+
+port_scan(){
+    echo -e "Starting Port Scanning (3/6)\n"
+    sed 's~^https://~~' active_urls.txt > raw_active_urls.txt
+    naabu -list raw_active_urls.txt -exclude-ports 80,443 -v -o ports.txt 
+    rm raw_active_urls.txt
+    echo -e "Finished Port Scanning\n"
+}
+
+gf(){
+    mkdir patterns; cd patterns
+    cat gau.txt | gf xss > possible_xss.txt
+    cat gau.txt | gf sqli > possible_sqli.txt
+    cat gau.txt | gf idor > possible_idor.txt
+    cat gau.txt | gf redirect > possible_idor.txt
+
+}
+
+
+# Instale as ferramentas de diferentes autores
+install_tools "tomnomnom" "assetfinder" "httprobe" "gf" "waybackurls"
+install_tools "projectdiscovery" "subfinder/v2/cmd/subfinder" "naabu/v2/cmd/naabu" "katana/v2/cmd/katana"
+install_tools "lc" "gau/v2/cmd/gau"
+install_tools "PentestPad" "subzy"
+
+subdomain_enum
+verify_subtakeover
+port_scan
+gather_urls
+gf
+
