714c83027303742efb1f5c0c8e1cd5808a2e9de8
Gabriel Pedro <gabrielpjehnen@hotmail.com>
2023-11-07 23:07:16 -0300

    fifth commit

--- lazyrec.sh
+++ lazyrec.sh
@@ -1,37 +1,48 @@
 #!/bin/bash
 
-#Author: Gabriel Jehnen
 
+display_banner() {
+    echo "====================================================="
+    figlet -f slant LAZYREC
+    echo "====================================================="
+    echo "Versão: 1.0"
+    echo "Desenvolvido por Gabriel Jehnen"
+    echo -e "=====================================================\n"
+}
+
+display_banner
 TARGETS_LIST=$1
 
-if [ "$#" -lt 1 ] || [ "$#" -gt 2 ]; then
-    echo "Usage: $0 <targets_file.txt> [foldername (optional)]"
-    exit 1
-elif ! test -f "$TARGETS_LIST" -o -r "$TARGETS_LIST"; then
-  echo "The first argument must be a text file."
-  echo "Usage: $0 <targets_file.txt> [foldername (optional)]"
-  exit 1
-fi
+#if [ "$#" -lt 1 ] || [ "$#" -gt 2 ]; then
+#    echo "Usage: $0 <targets_file.txt> [foldername (optional)]"
+#    exit 1
+#elif ! test -f "$TARGETS_LIST" -o -r "$TARGETS_LIST"; then
+#  echo "The first argument must be a text file."
+#  echo "Usage: $0 <targets_file.txt> [foldername (optional)]"
+#  exit 1
+#fi
 
 FOLDER_NAME=${2:-"lazyrec"}
 USERNAME=$(whoami)
 FOLDER_PATH=/home/$USERNAME/$FOLDER_NAME
 
+echo $1
+cat $1
 
 mkdir $FOLDER_PATH
-cp $0 $FOLDER_PATH
 cp $1 $FOLDER_PATH
 cd $FOLDER_PATH
 
+pwd
 
-configure_gf(){
+download_patterns(){
     cd ~; mkdir .gf; cd .gf;
     git clone https://github.com/1ndianl33t/Gf-Patterns.git;
     cd Gf-Patterns; mv * ..; rm -r Gf-Patterns;
     cd $FOLDER_PATH;
 }
 
-install_tools() {
+install_tools(){
     author="$1"
     shift
     for tool in "$@"; do
@@ -40,23 +51,22 @@ install_tools() {
         if ! command -v "$tool_name" &> /dev/null; then
             if $tool_name == "naabu"; then
                 sudo apt install -y libcap-dev
-            elif $tool_name == "gf"; then
-		echo "hehe"
-                if ls "~/.gf" 1> /dev/null 2>&1; then
-		    echo "hoho"
-                    configure_gf
-                fi
             fi
             echo "$tool_name não está instalado. Instalando..."
             go install -v "github.com/$author/$tool@latest"
         fi
+	if ! ls ~/.gf 1> /dev/null 2>&1; then
+	    download_patterns
+	fi
     done
+    pwd
 }
 
 subdomain_enum(){
     echo -e "Starting Subdomain Enumeration (1/6)\n"
+    meg /.well-known/security.txt --savestatus 200 $TARGETS_LIST &
     subfinder -dL $TARGETS_LIST -o subfinder.txt &
-    cat $TARGETS_LIST | assetfinder > assetfinder.txt &
+    cat $TARGETS_LIST | assetfinder --subs-only > assetfinder.txt &
     wait
     sort -u -o all_subs.txt subfinder.txt assetfinder.txt
     cat all_subs.txt | httprobe > active_urls.txt
@@ -84,9 +94,12 @@ port_scan(){
 }
 
 gf(){
-    cat gau.txt | gf xss > xss.txt
-    cat gau.txt | gf sqli > sqli.txt
-    cat gau.txt | gf redirect > redirect.txt
+    mkdir patterns
+    cat gau.txt | gf xss > patterns/xss.txt
+    cat gau.txt | gf sqli > patterns/sqli.txt
+    cat gau.txt | gf redirect > patterns/redirect.txt
+    cat gau.txt | gf idor > patterns/idor.txt
+    cat gau.txt | gf lfi > patterns/lfi.txt
 }
 
 
